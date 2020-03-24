32c72285d09451b74808c9f8edc6c3a3797ff95c
Bálint Sipos <sbalint2012@outlook.hu>
2020-03-24 10:47:28 +0100

    a

--- githubfeladat/githubfeladat/Form1.cs
+++ githubfeladat/githubfeladat/Form1.cs
@@ -15,6 +15,17 @@ namespace githubfeladat
         public Form1()
         {
             InitializeComponent();
+            label1.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible= true;
         }
     }
 }
